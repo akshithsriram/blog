b90a45a8516dab1222e379e3f25758c4d5c16439
akshithsriram <55990520+akshithsriram@users.noreply.github.com>
2020-08-11 10:04:55 +0530

    Add files via upload

---
+++ _action_files/run_notebooks.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+cd $(dirname "$0")/..
+cd _notebooks/
+
+ERRORS=""
+
+for file in *.ipynb
+do
+
+  if file == "2020-08-06-dynamic.ipynb"; then
+    if papermill --kernel python3 "${file}" "${file}"; then
+        echo "Sucessfully refreshed ${file}\n\n\n\n"
+        git add "${file}"
+    else
+        echo "ERROR Refreshing ${file}"
+        ERRORS="${ERRORS}, ${file}"
+    fi
+  fi
+done
+
+
+# Emit Errors If Exists So Downstream Task Can Open An Issue
+if [ -z "$ERRORS" ]
+then
+    echo "::set-output name=error_bool::false"
+else
+    echo "These files failed to update properly: ${ERRORS}"
+    echo "::set-output name=error_bool::true"
+    echo "::set-output name=error_str::${ERRORS}"
+fi
